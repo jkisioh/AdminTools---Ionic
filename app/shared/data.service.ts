c63eaac5fdda28d3c07ca794ed9e733bb77b5066
Jonah Kisioh <jkisioh@nb.rti.org>
2016-07-08 10:24:44 +0300

    Staging

---
+++ app/shared/data.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+/*PouchDB Connector*/
+
+@Injectable()
+export class DataService {
+  
+}
---
+++ app/shared/location.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { DataService } from 'shared';
+
+/*Use data serrivce to pull connection list*/
+
+@Injectable()
+export class LocationService {
+  
+  //get functions
+
+  //set functions
+}
